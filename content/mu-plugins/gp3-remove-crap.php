1445d4e02036e04ff40213c7b3d36dea7ab67a81
Guillem <git@guillemandreu.com>
2013-05-12 20:41:20 +0200

    This is not needed anymore

--- content/mu-plugins/gp3-remove-crap.php
+++ content/mu-plugins/gp3-remove-crap.php
@@ -6,19 +6,7 @@ Author: Guillem Andreu
 Author URI: http://guillemandreu.com
 Description: Remove automatic Wordpress crap that I don't want
 License: Proprietary - © 2013 Guillem Andreu
-Text Domain: gp3-remove-crap
 */
-
-
-/*
- * Localization
- */
-
-function gp3_remove_crap_lang() {
-	load_plugin_textdomain('gp3-remove-crap', false, basename(dirname(__FILE__)) . '/lang');
-}
-
-add_action('plugins_loaded', 'gp3_remove_crap_lang');
 
 
 
@@ -57,16 +45,3 @@ function gp3_remove_more_jump_link($link) {
 }
 
 add_filter('the_content_more_link', 'gp3_remove_more_jump_link');
-
-
-
-/*
- * Adding the ‘read more’ link to the_excerpt()
- */
-
-function gp3_add_excerpt_more_link($more) {
-	global $post;
-	return '… <a href="' . get_permalink($post->ID) . '" class="more-link">' . __('More', 'gp3-remove-crap') . ' »</a>';
-}
-
-add_filter('excerpt_more', 'gp3_add_excerpt_more_link');
