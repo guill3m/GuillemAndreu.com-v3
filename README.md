0d757504c1dc479da327fdc0c385f321ca1399d7
Guillem <git@guillemandreu.com>
2013-07-26 00:03:15 +0200

    Bio template

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # GuillemAndreu.com – V3
 
-I’m redesigning my professional website where I showcase my work. So, basically, this is a web portfolio. The CMS I’m using is [Wordpress](http://wordpress.org).
+## Work in progress
+
+My professional website where I showcase my work. So, basically, this is a **web portfolio**. The CMS I’m using is [Wordpress](http://wordpress.org), this is just the theme, and maybe some plugins.
 
 As it is also an element of my porfolio I thought it could be interesting to share the code and process.
 
-Feel free to use some bits of the code if it is useful to you, that’s why I put the code here on GitHub. Or use all of it, I don’t really care if you copy it all (shame on you), but it will be best for you if you don’t just copy and try to understand what’s going on with the code, how it works, and make it suit better to your needs.
+**Feel free to use some bits of the code if it is useful to you,** that’s why I put the code here on GitHub. Or use all of it, I don’t really care if you copy it all (shame on you), but it will be best for you if you don’t just copy and try to understand what’s going on with the code, how it works, and make it suit better to your needs and not mine.
 
 
 ## Plugins I’m using
@@ -21,4 +23,4 @@ All the custom plugins I made for this are on the repo. This is the list of plug
 
 - **I’m not supporting IE8-, only IE9+.** This is because the target of this website is most likely not going to be using that (stats from the current version prove that). Also, I want to play with some “modern” features and I don’t feel like using polyfills in this project right now. Maybe I’ll add support to IE8 in the future, but right now I’m more interested on keeping it lightweight and simple.
 
-- **I’m using Wordpress as a CMS, without the blog “module”.** I don’t need that for the moment. If some day I want to start a blog under this domain and design, then, I’ll develop whatever I need for that (I’m using custom post types for the portfolio, so the blog part is still there untoched, I prefer to do it this way).
+- **I’m using Wordpress as a CMS, without the “blog module”.** I don’t need that for the moment. If some day I want to start a blog under this domain and design, then, I’ll develop whatever I need for that (I’m using custom post types for the portfolio, so the blog part is still there untoched, I prefer to do it this way). Oh, by the way, I’ve changed the name of the content folder to just *content*, without the initial *wp-*, just because I can, it looks nicer also.
---
+++ content/themes/gp3/page-bio.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage GuillemAndreu.com
+ * @since GuillemAndreu.com 3.0.0
+ Template Name: Bio
+ */
+?>
+
+
+<?php get_header(); ?>
+
+<?php while (have_posts()) : the_post(); ?>
+
+	<article id="bio" class="bio">
+
+		<header id="bio-header" class="bio-header">
+			<h1 class="bio-name"><?php the_field('bio_name'); ?> <span class="bio-lastname"><?php the_field('bio_lastname'); ?></span></h1>
+			<div class="bio-picture">
+				<img src="<?php the_field('bio_picture'); ?>" alt="<?php _e('Picture', 'gp3') . echo ': ' . the_field('bio_name') . echo ' ' . the_field('bio_lastname'); ?>" />
+			</div>
+			<ul class="bio-details"><!--
+				--><li class="bio-birthday" data-icon="calendar"><?php the_field('bio_birthday'); ?></li><!--
+				--><li class="bio-location" data-icon="location"><?php the_field('bio_location'); ?>, <span class="bio-country"><?php the_field('bio_Country'); ?></span></li>
+			</ul>
+		</header>
+
+		<div class="bio-left">
+			<section id="bio-description" class="bio-description">
+				<?php the_content(); ?>
+			</section>
+
+			<?php if(has_sub_field('bio_experience')) : ?>
+				<div class="bio-left-left">
+					<section id="bio-experience">
+						<h2 class="bio-title"><?php _e('Experience', 'gp3'); ?></h2>
+						<ul>
+							<?php while(has_sub_field('bio_experience')) : ?>
+								<li class="bio-job">
+									<p class="bio-job-title"><span class="bio-company"><?php if(get_field('bio_job_company_url')) : ?><a href="<?php the_field('bio_job_company_url'); ?>" target="_blank"><?php endif; the_field('bio_job_company'); if(get_field('bio_job_company_url')) : ?></a><?php endif; ?></span> | <?php the_field('bio_job_title'); ?></p>
+									<p class="bio-job-description"><?php the_field('bio_job_description'); ?></p>
+									<p class="bio-job-lenght"><?php _e('From', 'gp3') . echo ' ' . the_field('bio_job_lenght_from'); if(get_field('bio_job_lenght_ended')) { _e('to', 'gp3') . echo ' ' . the_field('bio_job_lenght_ended'); } ?></p>
+								</li>
+							<?php endwhile; ?>
+						</ul>
+					</section>
+				</div>
+			<?php endif; ?>
+
+			<?php if(has_sub_field('bio_studies') || has_sub_field('bio_courses') || has_sub_field('bio_others')) : ?>
+				<div class="bio-left-right">
+
+					<?php if(has_sub_field('bio_studies')) : ?>
+						<section id="bio-studies">
+							<h2 class="bio-title"><?php _e('Studies', 'gp3'); ?></h2>
+							<ul>
+								<?php while(has_sub_field('bio_studies')) : ?>
+									<li class="bio-career">
+										<p class="bio-career-name"><?php the_field('bio_career_name'); ?></p>
+										<p class="bio-career-center"><?php if(get_field('bio_career_center_url')) : ?><a href="<?php the_field('bio_career_center_url'); ?>" target="_blank"><?php endif; the_field('bio_career_center'); if(get_field('bio_career_center_url')) : ?></a><?php endif; ?>, <span class="bio-career-ended"><?php the_field('bio_career_ended'); ?></span></p>
+									</li>
+								<?php endwhile; ?>
+							</ul>
+						</section>
+					<?php endif; ?>
+
+					<?php if(has_sub_field('bio_courses')) : ?>
+						<section id="bio-courses">
+							<h2 class="bio-title"><?php _e('Courses', 'gp3'); ?></h2>
+							<ul>
+								<?php while(has_sub_field('bio_courses')) : ?>
+									<li class="bio-course">
+										<p class="bio-course-name"><?php the_field('bio_course_name'); ?></p>
+										<p class="bio-course-center"><?php if(get_field('bio_course_center_url')) : ?><a href="<?php the_field('bio_course_center_url'); ?>" target="_blank"><?php endif; the_field('bio_course_center'); if(get_field('bio_course_center_url')) : ?></a><?php endif; ?>, <span class="course-lenght"><?php the_field('bio_course_lenght'); ?></span>. <span class="bio-course-date"><?php the_field('bio_course_date'); ?></span></p>
+									</li>
+								<?php endwhile; ?>
+							</ul>
+						</section>
+					<?php endif; ?>
+
+					<?php if(has_sub_field('bio_others')) : ?>
+						<section id="bio-others">
+							<h2 class="bio-title"><?php _e('Others', 'gp3'); ?></h2>
+							<ul>
+								<?php while(has_sub_field('bio_courses')) : ?>
+									<li class="bio-other">
+										<p class="bio-other-name"><?php the_field('bio_other_name'); ?></p>
+										<p class="bio-other-description"><?php the_field('bio_other_description'); ?></p>
+									</li>
+								<?php endwhile; ?>
+							</ul>
+						</section>
+					<?php endif; ?>
+
+				</div>
+			<?php endif; ?>
+
+		</div>
+
+		<?php if(has_sub_field('bio_languages') || has_sub_field('bio_skills') : ?>
+			<div class="bio-right">
+
+				<?php if(has_sub_field('bio_languages') : ?>
+					<section id="bio-languages">
+						<h2 class="bio-title"><?php _e('Languages', 'gp3'); ?></h2>
+						<ul>
+							<?php while(has_sub_field('bio_languages') : ?>
+								<li>
+									<p class="bio-skill level<?php the_field('bio_language_level'); ?>"><?php the_field('bio_language'); ?></p>
+									<p class="bio-language-level"><?php the_field('bio_language_level_txt'); ?></p>
+								</li>
+							<?php endwhile; ?>
+						</ul>
+					</section>
+				<?php endif; ?>
+
+				<?php if(has_sub_field('bio_skills') : ?>
+					<section id="bio-skills">
+						<h2 class="bio-title"><?php _e('Skills', 'gp3'); ?></h2>
+						<?php while(has_sub_field('bio_skillset') : ?>
+							<h3 class="bio-subtitle"><?php the_field('bio_skillset_title'); ?></h3>
+							<?php if(has_sub_field('bio_skillset_skills') : ?>
+								<ul>
+									<?php while(has_sub_field('bio_skillset_skills') : ?>
+										<li class="bio-skill level<?php the_field('bio_skill_level'); ?>"><?php the_field('bio_skill'); ?></li>
+									<?php endwhile; ?>
+								</ul>
+							<?php endif; ?>
+						<?php endwhile; ?>
+					</section>
+				<?php endif; ?>
+
+			</div>
+		<?php endif; ?>
+
+	</article>
+
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
