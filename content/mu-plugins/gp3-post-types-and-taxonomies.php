52ce015a4e021270b3cf1b7551c54b1c0c05a92b
Guillem <git@guillemandreu.com>
2013-07-29 05:18:58 +0200

    Now?

--- content/mu-plugins/gp3-post-types-and-taxonomies.php
+++ content/mu-plugins/gp3-post-types-and-taxonomies.php
@@ -6,7 +6,7 @@ Author: Guillem Andreu
 Author URI: http://guillemandreu.com
 Description: Custom post types and custom taxonomies
 License: Proprietary - © 2013 Guillem Andreu
-Text Domain: gp3-ptt
+Text Domain: gp3ptt
 */
 
 
@@ -15,7 +15,7 @@ Text Domain: gp3-ptt
  */
 
 function gp3_post_types_and_taxonomies_lang() {
-	load_plugin_textdomain('gp3-ptt', false, basename(dirname(__FILE__)) . '/lang');
+	load_plugin_textdomain('gp3ptt', false, basename(dirname(__FILE__)) . '/lang');
 }
 
 add_action('plugins_loaded', 'gp3_post_types_and_taxonomies_lang');
@@ -28,20 +28,20 @@ add_action('plugins_loaded', 'gp3_post_types_and_taxonomies_lang');
 
 function gp3_custom_post_types() {
 	$project_labels = array(
-		'name'               => __('Projects', 'gp3-ptt'),
-		'singular_name'      => __('Project', 'gp3-ptt'),
-		'menu_name'          => __('Projects', 'gp3-ptt'),
-		'all_items'          => __('All projects', 'gp3-ptt'),
-		'add_new'            => __('Add new', 'gp3-ptt'),
-		'add_new_item'       => __('Add new project', 'gp3-ptt'),
-		'edit'               => __('Edit', 'gp3-ptt'),
-		'edit_item'          => __('Edit project', 'gp3-ptt'),
-		'new_item'           => __('New project', 'gp3-ptt'),
-		'view_item'          => __('View project', 'gp3-ptt'),
-		'items_archive'      => __('Projects archive', 'gp3-ptt'),
-		'search_items'       => __('Search projects', 'gp3-ptt'),
-		'not_found'          => __('No projects found', 'gp3-ptt'),
-		'not_found_in_trash' => __('No projects found in the trash', 'gp3-ptt')
+		'name'               => __('Projects', 'gp3ptt'),
+		'singular_name'      => __('Project', 'gp3ptt'),
+		'menu_name'          => __('Projects', 'gp3ptt'),
+		'all_items'          => __('All projects', 'gp3ptt'),
+		'add_new'            => __('Add new', 'gp3ptt'),
+		'add_new_item'       => __('Add new project', 'gp3ptt'),
+		'edit'               => __('Edit', 'gp3ptt'),
+		'edit_item'          => __('Edit project', 'gp3ptt'),
+		'new_item'           => __('New project', 'gp3ptt'),
+		'view_item'          => __('View project', 'gp3ptt'),
+		'items_archive'      => __('Projects archive', 'gp3ptt'),
+		'search_items'       => __('Search projects', 'gp3ptt'),
+		'not_found'          => __('No projects found', 'gp3ptt'),
+		'not_found_in_trash' => __('No projects found in the trash', 'gp3ptt')
 	);
 	$project_supports = array(
 		'title',
@@ -51,13 +51,13 @@ function gp3_custom_post_types() {
 		'comments'
 	);
 	$project_rewrite = array(
-		'slug'       => __('portfolio', 'gp3-ptt'),
+		'slug'       => __('portfolio', 'gp3ptt'),
 		'with_front' => false,
 		'feeds'      => false
 	);
 	$project_args = array(
 		'labels'          => $project_labels,
-		'description'     => __('Portfolio items', 'gp3-ptt'),
+		'description'     => __('Portfolio items', 'gp3ptt'),
 		'public'          => true,
 		// 'menu_icon'       => basename(dirname(__FILE__)) . '/img/projects-icon.png',
 		'capability_type' => 'post',
@@ -82,19 +82,19 @@ function gp3_custom_post_types_messages($messages) {
 	global $post, $post_ID;
 	$messages['project'] = array(
 		0 => '', // Unused. Messages start at index 1.
-		1 => sprintf(__('Project updated. <a href="%s">View project</a>', 'gp3-ptt'), esc_url(get_permalink($post_ID))),
-		2 => __('Custom field updated.', 'gp3-ptt'),
-		3 => __('Custom field deleted.', 'gp3-ptt'),
-		4 => __('Project updated.', 'gp3-ptt'),
+		1 => sprintf(__('Project updated. <a href="%s">View project</a>', 'gp3ptt'), esc_url(get_permalink($post_ID))),
+		2 => __('Custom field updated.', 'gp3ptt'),
+		3 => __('Custom field deleted.', 'gp3ptt'),
+		4 => __('Project updated.', 'gp3ptt'),
 		/* translators: %s: date and time of the revision */
-		5 => isset($_GET['revision']) ? sprintf(__('Project restored to revision from %s', 'gp3-ptt'), wp_post_revision_title((int) $_GET['revision'], false)) : false,
-		6 => sprintf(__('Project published. <a href="%s">View project</a>', 'gp3-ptt'), esc_url(get_permalink($post_ID))),
-		7 => __('Project saved.', 'gp3-ptt'),
-		8 => sprintf(__('Project submitted. <a target="_blank" href="%s">Preview project</a>', 'gp3-ptt'), esc_url(add_query_arg('preview', 'true', get_permalink($post_ID)))),
-		9 => sprintf(__('Project scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview project</a>', 'gp3-ptt'),
+		5 => isset($_GET['revision']) ? sprintf(__('Project restored to revision from %s', 'gp3ptt'), wp_post_revision_title((int) $_GET['revision'], false)) : false,
+		6 => sprintf(__('Project published. <a href="%s">View project</a>', 'gp3ptt'), esc_url(get_permalink($post_ID))),
+		7 => __('Project saved.', 'gp3ptt'),
+		8 => sprintf(__('Project submitted. <a target="_blank" href="%s">Preview project</a>', 'gp3ptt'), esc_url(add_query_arg('preview', 'true', get_permalink($post_ID)))),
+		9 => sprintf(__('Project scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview project</a>', 'gp3ptt'),
 			// translators: Publish box date format, see http://php.net/date
 			date_i18n(__('M j, Y @ G:i'), strtotime($post->post_date)), esc_url(get_permalink($post_ID))),
-		10 => sprintf(__('Project draft updated. <a target="_blank" href="%s">Preview project</a>', 'gp3-ptt'), esc_url(add_query_arg( 'preview', 'true', get_permalink($post_ID)))),
+		10 => sprintf(__('Project draft updated. <a target="_blank" href="%s">Preview project</a>', 'gp3ptt'), esc_url(add_query_arg( 'preview', 'true', get_permalink($post_ID)))),
 	);
 	return $messages;
 }
@@ -109,25 +109,25 @@ add_filter( 'post_updated_messages', 'gp3_custom_post_types_messages');
 
 function gp3_custom_taxonomies() {
 	$type_labels = array(
-		'name'                  => __('Project types', 'gp3-ptt'),
-		'singular_name'         => __('Project type', 'gp3-ptt'),
-		'menu_name'             => __('Project types', 'gp3-ptt'),
-		'all_items'             => __('All project types', 'gp3-ptt'),
-		'edit_item'             => __('Edit project type', 'gp3-ptt'),
-		'view_item'             => __('View project type', 'gp3-ptt'),
-		'update_item'           => __('Update project type', 'gp3-ptt'),
-		'add_new_item'          => __('Add new project type', 'gp3-ptt'),
-		'new_item_name'         => __('New project type name', 'gp3-ptt'),
-		'parent_item'           => __('Parent project type', 'gp3-ptt'),
-		'parent_item_colon'     => __('Parent project type:', 'gp3-ptt'),
-		'search_items'          => __('Search project types', 'gp3-ptt'),
-		'popular_items'         => __('Popular project types', 'gp3-ptt'),
-		'add_or_remove_items'   => __('Add or remove project types', 'gp3-ptt'),
-		'choose_from_most_used' => __('Choose from most used project types', 'gp3-ptt'),
-		'not_found'             => __('No project types found', 'gp3-ptt')
+		'name'                  => __('Project types', 'gp3ptt'),
+		'singular_name'         => __('Project type', 'gp3ptt'),
+		'menu_name'             => __('Project types', 'gp3ptt'),
+		'all_items'             => __('All project types', 'gp3ptt'),
+		'edit_item'             => __('Edit project type', 'gp3ptt'),
+		'view_item'             => __('View project type', 'gp3ptt'),
+		'update_item'           => __('Update project type', 'gp3ptt'),
+		'add_new_item'          => __('Add new project type', 'gp3ptt'),
+		'new_item_name'         => __('New project type name', 'gp3ptt'),
+		'parent_item'           => __('Parent project type', 'gp3ptt'),
+		'parent_item_colon'     => __('Parent project type:', 'gp3ptt'),
+		'search_items'          => __('Search project types', 'gp3ptt'),
+		'popular_items'         => __('Popular project types', 'gp3ptt'),
+		'add_or_remove_items'   => __('Add or remove project types', 'gp3ptt'),
+		'choose_from_most_used' => __('Choose from most used project types', 'gp3ptt'),
+		'not_found'             => __('No project types found', 'gp3ptt')
 	);
 	$type_rewrite = array(
-		'slug'         => __('project-type', 'gp3-ptt'),
+		'slug'         => __('project-type', 'gp3ptt'),
 		'with_front'   => false,
 		'hierarchical' => true
 	);
